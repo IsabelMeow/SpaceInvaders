bdd5df55a2cc6a6100674a04b917dc29ddb1e131
IsabelMeow <isabelarias2018@gmail.com>
2022-12-10 20:36:23 -0500

    Changes

--- SpaceInvaders/app/src/main/java/edu/vanier/ufo/engine/GameEngine.java
+++ SpaceInvaders/app/src/main/java/edu/vanier/ufo/engine/GameEngine.java
@@ -154,7 +154,7 @@ public abstract class GameEngine {
                     Shape intersect = Shape.intersect((Shape)spriteA.getCollisionBounds(), (Shape)spriteB.getCollisionBounds()); 
                     if (spriteA instanceof Missile) {
                         Missile missile = ((Missile) spriteA); 
-                        missile.implode(this);
+                        
                     }
                     // The break helps optimize the collisions
                     //  The break statement means one object only hits another
